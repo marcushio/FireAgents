2007bff994f4c6eb2bdcc3af628caa23f82e410b
Colton Trujillo <coltonbtrujillo@cs.unm.edu>
2019-10-17 13:24:55 -0600

    create: Main class with boilerplate

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    /**
+     * Entry point to this javafx application.
+     *
+     * @param args A complete path to the configuration txt file that specifies initial conditions for the simulation.
+     *             The format will vary depending upon the user's OS and which directory they have the file in.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
