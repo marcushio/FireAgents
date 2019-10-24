7a21bb452b99b1f7dbe52f96d5e2f453c9026fbc
Colton Trujillo <coltonbtrujillo@cs.unm.edu>
2019-10-23 20:33:53 -0600

    implement: Station

--- src/Station.java
+++ src/Station.java
@@ -1,7 +1,20 @@
 public class Station {
     private Log log = new Log();
     private Coordinate coordinate;
+
+    /**
+     * Add entry to this Station's log
+     * @param entry LogEntry instance containing log info
+     */
     public void addToLog(LogEntry entry){
         log.addEntry(entry);
+    }
+
+    /**
+     * Set this instance's coordinate
+     * @param coordinate
+     */
+    public void setCoordinate(Coordinate coordinate){
+        this.coordinate = coordinate;
     }
 }
