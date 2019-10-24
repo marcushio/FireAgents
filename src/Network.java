7c66a42ccbbc11c9d35186411027047307670b41
Colton Trujillo <coltonbtrujillo@cs.unm.edu>
2019-10-23 21:14:44 -0600

    implement: public methods for setting Network's state

--- src/Network.java
+++ src/Network.java
@@ -1,9 +1,32 @@
+import java.util.Map;
+
 /**
  * @author: Marcus Trujillo
  * @version:
  * brief class description
  */
 public class Network {
+    private Map<Coordinate, Node> coordinateNodeMap;
+    private Station station;
+
+    public void put(Coordinate coordinate, Node node){
+        coordinateNodeMap.put(coordinate,node);
+    }
+    public boolean setFire(Coordinate coordinate){
+        Node node;
+        if((node = coordinateNodeMap.get(coordinate))!= null){
+            node.ignite();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setStation(Station station){
+        if (this.station!=null) return false;
+        this.station = station;
+        return true;
+    }
+
     //this is probably going to be the home of our threadpool cached is likely going to be our choice
 
 
